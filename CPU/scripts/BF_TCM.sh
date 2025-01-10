8751258fb87c2a250ee905579c2e2e51ef429a1e
Hanwen808 <164186121+Hanwen808@users.noreply.github.com>
2025-01-10 10:15:43 +0800

    Add files via upload

---
+++ CPU/scripts/BF_TCM.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# options
+optimize_flag="-O3"
+
+# compile
+echo "Compiling BF+TCM with $optimize_flag optimization flag..."
+g++ ../Sources/bitmap.cpp ../Sources/MurmurHash3.cpp ../Sources/BF_TCM.cpp ../Sources/TCM.cpp ../BF_TCM_main.cpp -o BF_TCM -std=c++17 -I ../C++ $optimize_flag
+
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Running BF+TCM..."
+    ./BF_TCM
+else
+    echo "Compilation failed. Exiting."
+    exit 1
+fi
